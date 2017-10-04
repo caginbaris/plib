340d0ee17589ace16eb1fdb6fa113f7e61cb1c6a
caginbaris <31400655+caginbaris@users.noreply.github.com>
2017-10-04 17:16:04 +0300

    delay_algorithms added

--- plib/protection_functions.c
+++ plib/protection_functions.c
@@ -1,12 +1,84 @@
 
+#include "protection_functions.h"
 
-//function-1
-
-
-int on_delay(){
-
+//function-1,2,3
+//count  has to be initialized and supplied as different in each invocation
 
 
 
+int on_delay(unsigned int input, unsigned int mem, unsigned int qual_sample,unsigned int *count){
+
+	int out;
+
+	out=mem;
+
+	if (input==1 && mem==0 )
+	{
+		*count=*count+1;
+
+		if ((*count)==qual_sample)
+		{
+			out = 1;
+			*count = 0;
+		}
+
+	}
+	
+	else{*count=0;}
+
+	if (input==0){out = 0; *count=0;}
+
+	return out;
+
+}
+
+int off_delay(unsigned int input, unsigned int mem, unsigned int qual_sample,unsigned int *count){
+
+	int out;
+
+	out=mem;
+
+	if (input==0 && mem==1 )
+	{
+		*count=*count+1;
+
+		if ((*count)==qual_sample)
+		{
+			out = 0;
+			*count = 0;
+		}
+
+	}
+	
+	else{*count=0;}
+
+	if (input==1){out = 1; *count=0;}
+
+	return out;
+
+}
+
+int on_off_delay(unsigned int input, unsigned int mem, unsigned int qual_sample,unsigned int *count){
+
+	int out;
+
+	out=mem;
+
+	if (input^mem )
+	{
+		*count=*count+1;
+
+		if ((*count)==qual_sample)
+		{
+			out = input;
+			*count = 0;
+		}
+
+	}
+	
+	else{*count=0;}
+
+	
+	return out;
 
 }
---
+++ plib/protection_functions.h
@@ -0,0 +1,6 @@
+
+
+
+int on_delay(unsigned int input, unsigned int mem, unsigned int qual_sample,unsigned int *count);
+int off_delay(unsigned int input, unsigned int mem, unsigned int qual_sample,unsigned int *count);
+int on_off_delay(unsigned int input, unsigned int mem, unsigned int qual_sample,unsigned int *count);
