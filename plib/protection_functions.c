ceb4d0a783b506d95f91b1d2ef8f47c335d3cc74
caginbaris <31400655+caginbaris@users.noreply.github.com>
2017-10-06 10:32:25 +0300

    Inverse TOC added

--- plib/protection_functions.c
+++ plib/protection_functions.c
@@ -1,5 +1,6 @@
 
 #include "protection_functions.h"
+#include "math.h"
 
 //function-1,2,3
 //count  has to be initialized and supplied as different in each invocation
@@ -85,25 +86,82 @@ int on_off_delay(unsigned int input, unsigned int mem, unsigned int qual_sample,
 //function-4
 // Definite TOC
 
-int fc50(struct fc50_inputParameters fc50_in, struct fc50_outputParameters *fc50_out ){
+int fc50(struct fc50_inputParameters fc50_in, struct fc50_outputParameters *fc50_out, int enable ){
 
 
-	if(fc50_in.rms>fc50_in.level){ fc50_out->initial_pick_up=1;}
-	if(fc50_in.rms<fc50_in.level*fc50_in.dropout_ratio){ fc50_out->initial_pick_up=0;}
-	
-	fc50_out->pick_up=off_delay(	fc50_out->initial_pick_up,
-								   	fc50_out->pick_up,
-									fc50_in.dropout_time,
-									&(fc50_out->dropout_counter));
+	if (enable)
+	{
 
-	fc50_out->trip=		on_delay(	fc50_out->pick_up,
-								   	fc50_out->trip,
-									fc50_in.delay,
-									&(fc50_out->trip_counter));
+		if (fc50_in.rms > fc50_in.level)
+		{
+			fc50_out->initial_pick_up = 1;
+		}
+		if (fc50_in.rms < fc50_in.level * fc50_in.dropout_ratio)
+		{
+			fc50_out->initial_pick_up = 0;
+		}
 
-	if(fc50_out->trip==1){fc50_out->trip_latch=1;}
+		fc50_out->pick_up = off_delay(fc50_out->initial_pick_up, fc50_out->pick_up, fc50_in.dropout_time, &(fc50_out->dropout_counter));
+
+		fc50_out->trip = on_delay(fc50_out->pick_up, fc50_out->trip, fc50_in.delay, &(fc50_out->trip_counter));
+
+		if (fc50_out->trip == 1)
+		{
+			fc50_out->trip_latch = 1;
+		}
+
+	}
 
 	return fc50_out->trip_latch;
+
+}
+
+//function-4
+// Inverse TOC
+
+
+int fc51(struct fc51_inputParameters fc51_in, struct fc51_outputParameters *fc51_out,int enable ){
+
+	if (enable)
+	{
+
+		if (fc51_in.rms > fc51_in.level * 1.100f)
+		{
+			fc51_out->pick_up = 1;
+		}
+		if (fc51_in.rms < fc51_in.level * 1.045f)
+		{
+			fc51_out->pick_up = 0;
+		}
+
+		fc51_out->time2trip = fc51_in.time_multiplier * (fc51_in.curve_data[0] / (powf((fc51_in.rms / fc51_in.level), fc51_in.curve_data[1]) - 1.0f) + fc51_in.curve_data[2]);
+		fc51_out->sample2trip = fc51_out->time2trip * fs;
+
+		if (fc51_out->pick_up)
+		{
+
+			fc51_out->trip_counter++;
+
+		}
+		else
+		{
+
+			fc51_out->trip_counter = 0;
+
+		};
+
+		if (fc51_out->trip_counter > fc51_out->sample2trip)
+		{
+
+			fc51_out->trip = 1;
+
+		}
+
+
+
+	}
+
+	return fc51_out->trip ;
 
 }
 
--- plib/protection_functions.h
+++ plib/protection_functions.h
@@ -1,32 +1,62 @@
+#define fs 2500
 
+int on_delay(unsigned int input, unsigned int mem, unsigned int qual_sample, unsigned int *count);
+int off_delay(unsigned int input, unsigned int mem, unsigned int qual_sample, unsigned int *count);
+int on_off_delay(unsigned int input, unsigned int mem, unsigned int qual_sample, unsigned int *count);
 
+//------Definite Time Overcurrent-Start--------------------------------------------------------------------
+struct fc50_inputParameters {
 
-int on_delay(unsigned int input, unsigned int mem, unsigned int qual_sample,unsigned int *count);
-int off_delay(unsigned int input, unsigned int mem, unsigned int qual_sample,unsigned int *count);
-int on_off_delay(unsigned int input, unsigned int mem, unsigned int qual_sample,unsigned int *count);
+	float rms;
+	float level;
+	float dropout_ratio;
 
-
-struct fc50_inputParameters{
-
-float rms;
-float level;
-float delay;
-float dropout_ratio;
-float dropout_time;
+	unsigned int delay;
+	unsigned int dropout_time;
 
 };
-struct fc50_outputParameters{
+struct fc50_outputParameters {
 
-unsigned int initial_pick_up:1;
-unsigned int pick_up:1;
-unsigned int trip:1;
-unsigned int trip_latch:1;
+	unsigned int initial_pick_up:1;
+	unsigned int pick_up:1;
+	unsigned int trip:1;
+	unsigned int trip_latch:1;
 
-
-unsigned int dropout_counter;
-unsigned int trip_counter;
-
+	unsigned int dropout_counter;
+	unsigned int trip_counter;
 
 };
 
-int fc50(struct fc50_inputParameters fc50_in, struct fc50_outputParameters *fc50_out );
+int fc50(struct fc50_inputParameters fc50_in, struct fc50_outputParameters *fc50_out, int enable);
+//------Definite Time Overcurrent-End---------------------------------------------------------------------
+
+
+//------Inverse Time Overcurrent-Start--------------------------------------------------------------------
+struct fc51_inputParameters {
+
+	float rms;
+	float level;
+	float dropout_ratio;
+	float curve_data[3];
+	float time_multiplier;
+
+
+
+};
+struct fc51_outputParameters {
+
+	float time2trip;
+	long sample2trip;
+	long trip_counter;
+	
+	unsigned int pick_up:1;
+	unsigned int trip:1;
+
+
+
+};
+
+int fc51(struct fc51_inputParameters fc51_in, struct fc51_outputParameters *fc51_out,int enable );
+
+//------Inverse Time Overcurrent-End--------------------------------------------------------------------
+
