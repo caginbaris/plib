79be0b4c71186fd6f691b58126b707ff4df05aad
caginbaris <31400655+caginbaris@users.noreply.github.com>
2017-10-07 17:04:57 +0300

    pib tested up to fc59(ok) without BF

--- plib/protection_functions.c
+++ plib/protection_functions.c
@@ -168,21 +168,22 @@ int fc51(struct fc51_inputParameters fc51_in, struct fc51_outputParameters *fc51
 
 //function-6
 //Undervoltage Protection
-// cs trip caution......
+// cs trip caution...... (has to set externally by trigger of trip)
+
 
 
 int fc27(struct fc27_inputParameters fc27_in, struct fc27_outputParameters *fc27_out,int enable ){
 
 	if (enable){
 
-		if (fc27_in.rms < fc27_in.level ){
+		if (fc27_in.rms < fc27_in.level && fc27_in.cs==0 ){
 		
 			fc27_out->pick_up = 1;
 		
 		}
 
 
-		if (fc27_in.rms > fc27_in.level*fc27_in.dropout_ratio ){
+		if (fc27_in.rms > fc27_in.level*fc27_in.dropout_ratio || fc27_in.cs ){
 		
 			fc27_out->pick_up = 0;
 		
@@ -240,7 +241,6 @@ int fc59(struct fc59_inputParameters fc59_in, struct fc59_outputParameters *fc59
 		}
 
 
-
 		if (fc59_out->pick_up && fc59_out->trip==0){
 
 			fc59_out->trip_counter++;
@@ -266,6 +266,383 @@ int fc59(struct fc59_inputParameters fc59_in, struct fc59_outputParameters *fc59
 	return fc59_out->trip;
 
 }
+
+
+// function-8
+// Definite TOC Negative Seq. Protection
+
+int fc46d(struct fc46d_inputParameters fc46d_in, struct fc46d_outputParameters *fc46d_out, int enable ){
+
+
+	if (enable && (fc46d_in.rms>0.1f*fc46d_in.level && fc46d_in.rms<10.0f*fc46d_in.level  ))
+	{
+
+		if (fc46d_in.rms > fc46d_in.level)
+		{
+			fc46d_out->initial_pick_up = 1;
+		}
+		if (fc46d_in.rms < fc46d_in.level * fc46d_in.dropout_ratio)
+		{
+			fc46d_out->initial_pick_up = 0;
+		}
+
+		fc46d_out->pick_up = off_delay(fc46d_out->initial_pick_up, fc46d_out->pick_up, fc46d_in.dropout_time*fs, &(fc46d_out->dropout_counter));
+
+		fc46d_out->trip = on_delay(fc46d_out->pick_up, fc46d_out->trip, fc46d_in.delay*fs, &(fc46d_out->trip_counter));
+
+		if (fc46d_out->trip == 1)
+		{
+			fc46d_out->trip_latch = 1;
+		}
+
+	}
+
+	return fc46d_out->trip_latch;
+
+}
+
+
+// function-9
+// Inverse TOC Negative Seq. Protection
+
+int fc46i(struct fc46i_inputParameters fc46i_in, struct fc46i_outputParameters *fc46i_out,int enable ){
+
+	if (enable && (fc46i_in.rms>0.1f*fc46i_in.level && fc46i_in.rms<10.0f*fc46i_in.level  )){
+
+		if (fc46i_in.rms > fc46i_in.level * 1.100f){
+
+			fc46i_out->pick_up = 1;
+			fc46i_out->time2trip = fc46i_in.time_multiplier * (fc46i_in.curve_data[0] / (powf((fc46i_in.rms / fc46i_in.level), fc46i_in.curve_data[1]) - 1.0f) + fc46i_in.curve_data[2]);
+			
+
+		}
+		if (fc46i_in.rms < fc46i_in.level * 1.045f){
+
+			fc46i_out->pick_up = 0;
+			fc46i_out->time2trip =80000; 
+			
+		}
+
+
+
+		if (fc46i_out->pick_up && fc46i_out->trip ==0){
+
+			fc46i_out->trip_counter++;
+
+		}else{
+
+			fc46i_out->trip_counter = 0;
+
+		};
+
+		if (fc46i_out->trip_counter > fc46i_out->time2trip * fs){
+
+			fc46i_out->trip = 1;
+			fc46i_out->trip_counter = 0;
+
+		}
+
+
+
+	}
+
+	return fc46i_out->trip ;
+
+}
+
+// function-10
+// Overtempereature Protection
+
+void fc49(struct fc49_inputParameters fc49_in, struct fc49_outputParameters *fc49_out,int enable ){
+
+	if (enable){
+
+		if (fc49_in.temp > fc49_in.alarm_level ){
+		
+			fc49_out->alarm = 1;
+		
+		}
+
+
+		if (fc49_in.temp < fc49_in.alarm_level*fc49_in.dropout_ratio ){
+		
+			fc49_out->alarm = 0;
+		
+		}
+
+
+		if (fc49_in.temp > fc49_in.trip_level ){
+		
+			fc49_out->trip = 1;
+		
+		}
+
+	
+	}
+
+
+}
+
+
+// function-11
+// Breaker Failure Protection
+
+
+
+// function-12
+// Undercurrent Protection
+// bs is CB input
+
+
+int fc37(struct fc37_inputParameters fc37_in, struct fc37_outputParameters *fc37_out,int enable){
+
+	if (enable){
+
+		if (fc37_in.rms < fc37_in.level && fc37_in.bs ){
+		
+			fc37_out->pick_up = 1;
+		
+		}
+
+
+		if (fc37_in.rms > fc37_in.level*fc37_in.dropout_ratio || fc37_in.bs==0 ){
+		
+			fc37_out->pick_up = 0;
+		
+		}
+
+
+
+		if (fc37_out->pick_up && fc37_out->trip==0){
+
+			fc37_out->trip_counter++;
+
+		}else{
+
+			fc37_out->trip_counter = 0;
+
+		}
+
+
+
+		if (fc37_out->trip_counter > fc37_in.delay*fs){
+
+			fc37_out->trip = 1;
+			fc37_out->trip_counter = 0;
+
+		}
+
+	
+	}
+
+	return fc37_out->trip;
+
+}
+
+// Function 14
+// Filter Unbalance Protection-Alarm Stage
+// caution : vectoral difference has to be used
+
+
+int fcUNBd(struct fcUNBd_inputParameters fcUNBd_in, struct fcUNBd_outputParameters *fcUNBd_out,int enable ){
+
+	if (enable){
+
+		if (fcUNBd_in.rms > fcUNBd_in.level ){
+		
+			fcUNBd_out->pick_up = 1;
+		
+		}
+
+
+		if (fcUNBd_in.rms < fcUNBd_in.level*fcUNBd_in.dropout_ratio ){
+		
+			fcUNBd_out->pick_up = 0;
+		
+		}
+
+
+		if (fcUNBd_out->pick_up && fcUNBd_out->trip==0){
+
+			fcUNBd_out->trip_counter++;
+
+		}else{
+
+			fcUNBd_out->trip_counter = 0;
+
+		}
+
+
+		if (fcUNBd_out->trip_counter > fcUNBd_in.delay*fs){
+
+			fcUNBd_out->trip = 1;
+			fcUNBd_out->trip_counter = 0;
+
+		}
+
+	
+	}
+
+	return fcUNBd_out->trip;
+
+}
+
+
+// Function 15
+// Filter Unbalance Protection-Trip Stage
+// caution : vectoral difference has to be used
+
+
+int fcUNBi(struct fcUNBi_inputParameters fcUNBi_in, struct fcUNBi_outputParameters *fcUNBi_out,int enable ){
+
+	if (enable ){
+
+		if (fcUNBi_in.rms > fcUNBi_in.level * 1.100f){
+
+			fcUNBi_out->pick_up = 1;
+			fcUNBi_out->time2trip = fcUNBi_in.time_multiplier * (fcUNBi_in.curve_data[0] / (100.0f*(fcUNBi_in.rms/fcUNBi_in.level)-97.0f)+0.02);
+			
+
+		}
+		if (fcUNBi_in.rms < fcUNBi_in.level * 1.045f){
+
+			fcUNBi_out->pick_up = 0;
+			fcUNBi_out->time2trip =80000; 
+			
+		}
+
+
+
+		if (fcUNBi_out->pick_up && fcUNBi_out->trip ==0){
+
+			fcUNBi_out->trip_counter++;
+
+		}else{
+
+			fcUNBi_out->trip_counter = 0;
+
+		};
+
+		if (fcUNBi_out->trip_counter > fcUNBi_out->time2trip * fs){
+
+			fcUNBi_out->trip = 1;
+			fcUNBi_out->trip_counter = 0;
+
+		}
+
+
+
+	}
+
+	return fcUNBi_out->trip ;
+
+}
+
+
+
+// Function 16
+// Filter PVP definite
+
+
+
+int fcPVPd(struct fcPVPd_inputParameters fcPVPd_in, struct fcPVPd_outputParameters *fcPVPd_out,int enable ){
+
+	if (enable){
+
+		if (fcPVPd_in.rms > fcPVPd_in.level ){
+		
+			fcPVPd_out->pick_up = 1;
+		
+		}
+
+
+		if (fcPVPd_in.rms < fcPVPd_in.level*fcPVPd_in.dropout_ratio ){
+		
+			fcPVPd_out->pick_up = 0;
+		
+		}
+
+
+		if (fcPVPd_out->pick_up && fcPVPd_out->trip==0){
+
+			fcPVPd_out->trip_counter++;
+
+		}else{
+
+			fcPVPd_out->trip_counter = 0;
+
+		}
+
+
+		if (fcPVPd_out->trip_counter > fcPVPd_in.delay*fs){
+
+			fcPVPd_out->trip = 1;
+			fcPVPd_out->trip_counter = 0;
+
+		}
+
+	
+	}
+
+	return fcPVPd_out->trip;
+
+}
+
+
+// Function 17
+// Filter PVP-Trip Stage
+
+
+
+int fcPVPi(struct fcPVPi_inputParameters fcPVPi_in, struct fcPVPi_outputParameters *fcPVPi_out,int enable ){
+
+	if (enable ){
+
+		if (fcPVPi_in.rms > fcPVPi_in.level * 1.100f){
+
+			fcPVPi_out->pick_up = 1;
+			fcPVPi_out->time2trip = fcPVPi_in.time_multiplier * (fcPVPi_in.curve_data[0] / (100.0f*(fcPVPi_in.rms/fcPVPi_in.level)-97.0f)+0.02);
+			
+
+		}
+		if (fcPVPi_in.rms < fcPVPi_in.level * 1.045f){
+
+			fcPVPi_out->pick_up = 0;
+			fcPVPi_out->time2trip =80000; 
+			
+		}
+
+
+
+		if (fcPVPi_out->pick_up && fcPVPi_out->trip ==0){
+
+			fcPVPi_out->trip_counter++;
+
+		}else{
+
+			fcPVPi_out->trip_counter = 0;
+
+		};
+
+		if (fcPVPi_out->trip_counter > fcPVPi_out->time2trip * fs){
+
+			fcPVPi_out->trip = 1;
+			fcPVPi_out->trip_counter = 0;
+
+		}
+
+
+
+	}
+
+	return fcPVPi_out->trip ;
+
+}
+
+
+
+
+
 
 
 
--- plib/protection_functions.h
+++ plib/protection_functions.h
@@ -51,7 +51,6 @@ struct fc51_outputParameters {
 	unsigned int trip:1;
 
 
-
 };
 
 int fc51(struct fc51_inputParameters fc51_in, struct fc51_outputParameters *fc51_out,int enable );
@@ -66,8 +65,7 @@ struct fc27_inputParameters{
 	float level;
 	float dropout_ratio;
 	float delay;
-	float dropout_time;
-	unsigned int cs;
+	unsigned int cs; // caution: externally set by a routine
 
 };
 
@@ -93,7 +91,6 @@ struct fc59_inputParameters{
 	float level;
 	float dropout_ratio;
 	float delay;
-	float dropout_time;
 	unsigned int cs;
 
 };
@@ -108,4 +105,192 @@ struct fc59_outputParameters{
 };
 
 int fc59(struct fc59_inputParameters fc59_in, struct fc59_outputParameters *fc59_out,int enable );
+
+//------OverVoltageProtection-End--------------------------------------------------------------------
+
+
+//------NegativeSequenceProtection-Start------------------------------------------------------------
+
+struct fc46d_inputParameters{
+
+	float rms;
+	float level;
+	float delay;
+	float dropout_time;
+	float dropout_ratio;
+
+};
+
+
+struct fc46d_outputParameters{
+
+	
+	long trip_counter;
+	long dropout_counter;
+
+	unsigned int initial_pick_up:1;
+	unsigned int pick_up:1;
+	unsigned int trip:1;
+	unsigned int trip_latch:1;
+
+};
+
+
+
+struct fc46i_inputParameters {
+
+	float rms;
+	float level;
+	float curve_data[3];
+	float time_multiplier;
+
+};
+struct fc46i_outputParameters {
+
+	float time2trip;
+	long trip_counter;
+	
+	unsigned int pick_up:1;
+	unsigned int trip:1;
+
+
+};
+
+//------NegativeSequenceProtection-End---------------------------------------------------
+
+
+
+//------ThermalProtection-Start------------------------------------------------------------
+
+struct fc49_inputParameters{
+
+	float temp;
+	float alarm_level; 
+	float trip_level; 
+	float dropout_ratio;
+
+};
+
+
+struct fc49_outputParameters{
+
+	
+	unsigned int alarm:1;
+	unsigned int trip:1;
+	
+
+};
+
+//------ThermalProtection-End------------------------------------------------------------
+
+
+//------Breaker Failure-Start------------------------------------------------------------
+//------Breaker Failure-End--------------------------------------------------------------
+
+
+//------UnderCurrent-Start------------------------------------------------------------
+struct fc37_inputParameters{
+
+	float rms;
+	float level;
+	float dropout_ratio;
+	float delay;
+	unsigned int bs; // will be assigned to CB position
+
+};
+
+
+struct fc37_outputParameters{
+
+	long trip_counter;
+	unsigned int pick_up:1;
+	unsigned int trip:1;
+
+};
+
+//------UnderCurrent-End--------------------------------------------------------------
+
+//------Unbalace-Start------------------------------------------------------------
+
+struct fcUNBd_inputParameters{
+
+	float rms;
+	float level; // vectoral difference will be used
+	float dropout_ratio;
+	float delay;
+	
+
+};
+
+struct fcUNBd_outputParameters{
+
+	long trip_counter;
+	unsigned int pick_up:1;
+	unsigned int trip:1;
+
+};
+
+
+
+struct fcUNBi_inputParameters {
+
+	float rms;
+	float level;
+	float curve_data[3];
+	float time_multiplier;
+
+	
+};
+
+
+struct fcUNBi_outputParameters{
+
+	float time2trip;
+	long trip_counter;
+	
+	unsigned int pick_up:1;
+	unsigned int trip:1;
+};
+
+//------Unbalace-Start------------------------------------------------------------
+
+struct fcPVPd_inputParameters{
+
+	float rms;
+	float level; // vectoral difference will be used
+	float dropout_ratio;
+	float delay;
+	
+
+};
+
+struct fcPVPd_outputParameters{
+
+	long trip_counter;
+	unsigned int pick_up:1;
+	unsigned int trip:1;
+
+};
+
+
+struct fcPVPi_inputParameters {
+
+	float rms;
+	float level;
+	float curve_data[3];
+	float time_multiplier;
+
+
+};
+
+
+struct fcPVPi_outputParameters{
+
+	float time2trip;
+	long trip_counter;
+	
+	unsigned int pick_up:1;
+	unsigned int trip:1;
+};
+
 
